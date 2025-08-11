be3f7306a927334e859f2b586b6be787a8276d78
itskheila <munenetiara@gmail.com>
2025-08-11 09:14:33 +0300

    python basics commit

---
+++ classes/getters_setters.py
@@ -0,0 +1,54 @@
+#Speical method in a class
+#initializer: method is called whenver the blueprint is used
+#To create an object
+#method function is inside a class
+
+# This code is a simple implementation of a class in Python with an initializer, properties, and methods.
+# It includes a method to write to a file and demonstrates the use of properties in a class
+# The class `Human` has an initializer that sets properties
+
+
+
+from datetime import datetime
+
+def write_file(f_name,txt):
+    with open(f_name,'a') as file:
+        file.write(f"{txt} \n")
+       
+class Human():
+
+    def __init__(self,gender,name):
+        print("The initializer wass called")
+        self.gender=gender
+        self.name=name
+        if self.gender=="Male":
+            self.ribs=24
+            self.curse="Suffer"
+        else :
+          self.ribs=23
+          self.curse="Pain"
+
+    def get_name(self):
+        now = datetime.now()
+        print("Curreent date and time",now)
+        write_file(f_name="log.txt",txt=f"At {now} got name from adam")
+        return self.name
+
+    def print_self(self):
+        print("----------------------")
+        print("name",self.name)
+        print("gender",self.gender)
+        print("ribs",self.ribs)
+        print("curse",self.curse)
+        print("---------------------")
+
+
+# adam=Human(name="adam",gender="Male") #object from a class
+adam=Human(name="adam",gender="Male")
+
+#Getter a property of: <name>:
+# print(adam.name)
+
+print(adam.get_name())
+
+# @property
